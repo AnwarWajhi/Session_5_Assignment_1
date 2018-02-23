c89aae1307f4a3bcae97a6bce69eb14edd30d38e
AnwarWajhi <anwarwajhi5@gmail.com>
2018-02-23 23:32:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var name = "Anwar Wajhi"; 
+var age  = 26; 
+var dob = "05 Aug 1991";
+var pob = "Hyderabad";
+console.log("Name: "+ name
+             + "\n Age: "+ age
+             +"\n Date of birth: " + dob
+             + "\n Place of birth: " + pob );
